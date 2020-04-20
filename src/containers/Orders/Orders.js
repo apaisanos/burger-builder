564718baab9c190a3fe64db4f67581e45dac99c6
Andreas Paisanos <apaisanos@Andreass-MacBook-Air.local>
2020-04-20 16:41:53 +0300

    Finished orders page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
 
@@ -13,8 +14,14 @@ class Orders extends Component {
 
         axios.get('/orders.json')
             .then(res => {
-                console.log(res.data);
-                this.setState({loading: false});
+                const fetchedOrders = [];
+                for(let key in res.data){
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({loading: false, orders: fetchedOrders});
             })
             .catch(err => {
                 this.setState({loading: false});
@@ -24,11 +31,16 @@ class Orders extends Component {
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
